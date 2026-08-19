952574f9cf34e1c5afff9742531707d01781203d
Henry Li <hl3547@princeton.edu>
2026-08-19 17:13:15 -0400

    haven

--- src/components/Design/Haven.jsx
+++ src/components/Design/Haven.jsx
@@ -14,28 +14,41 @@ import Carousel from '../Carousel.jsx';
 export default function Haven() {
   return (
     <div className='min-h-screen flex flex-col p-6'>
-      <Nav />
-      <div className='pt-24'>
-      <div className='w-full flex justify-between items-center'>
-                <h2 className='relative md:-left-2'>Tang</h2>
-              </div>
+    <Nav />
 
-              <div className='flex flex-col gap-1'>
+      <div className='border-r border-l border-t flex-1 flex flex-col items-center md:px-0 px-6 py-48 md:py-40'>
+        <div className='w-full max-w-3xl flex flex-col gap-8'>
 
-                <h3>Role: Brand Designer</h3>
-                <h3>Timeline: 3 days</h3>
-                <h3>Tools: Adobe Illustrator, Photoshop</h3>
-              </div>
+          <div className='flex flex-col gap-1'>
+            <h2 className='relative'>Haven</h2>
 
-        <img src={intro} />
-        <img src={two} />
-        <img src={three} />
-        <img src={four} />
-        <img src={five} />
-        <img src={six} />
+            <h3>Role: Brand Designer</h3>
+            <h3>Timeline: 3 days</h3>
+            <h3>Tools: Adobe Illustrator, Photoshop</h3>
+
+
+          </div>
+          <a href='https://www.figma.com/proto/UHS2PawiamcWey8Qb3A3oM/Haven?node-id=266-615&p=f&t=BSUzhhetran2Drub-1&scaling=contain&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=12%3A93&show-proto-sidebar=1'
+              target='_blank'
+              rel='noreferrer'
+              className='px-8 py-4 border w-fit'
+            >
+              Figma Prototype
+              </a>
+
+        <div>
+          <img src={intro} />
+          <img src={two} />
+          <img src={three} />
+          <img src={four} />
+          <img src={five} />
+          <img src={six} />
+          </div> 
+
       </div>
 
       <Footer />
+    </div>
     </div>
   )
 }
